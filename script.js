bb435383186c26a02f54d5a5393d98cde060df47
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2024-09-24 14:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 // Your script here.
+const msg = new SpeechSynthesisUtterance();
+      let voices = [];
+      const voicesDropdown = document.querySelector('[name="voice"]');
+      const options = document.querySelectorAll(
+        '[type="range"], [name="text"]'
+      );
+      const speakButton = document.querySelector("#speak");
+      const stopButton = document.querySelector("#stop");
+
+      // Populating the voice options
+      function populateVoices() {
+        voices = speechSynthesis.getVoices();
+        voicesDropdown.innerHTML = voices
+          .map(
+            (voice) =>
+              `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+          )
+          .join("");
+      }
+
+      // Set the selected voice
+      function setVoice() {
+        msg.voice = voices.find((voice) => voice.name === this.value);
+        toggle();
+      }
+
+      // Toggle speaking
+      function toggle(startOver = true) {
+        speechSynthesis.cancel(); // Stop any current speech
+        if (startOver) {
+          msg.text = document.querySelector('[name="text"]').value;
+          speechSynthesis.speak(msg);
+        }
+      }
+
+      // Setting options for rate and pitch
+      function setOption() {
+        msg[this.name] = this.value;
+        toggle(false); // Don't restart speaking when options are changed
+      }
+
+      speechSynthesis.addEventListener("voiceschanged", populateVoices);
+      voicesDropdown.addEventListener("change", setVoice);
+      options.forEach((option) => option.addEventListener("change", setOption));
+      speakButton.addEventListener("click", toggle);
+      stopButton.addEventListener("click", () => toggle(false)); // Stop speaking
